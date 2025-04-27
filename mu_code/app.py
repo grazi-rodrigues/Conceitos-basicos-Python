c1105aec20529617d5f45807878c2cf79318c7a4
Graziele Rodrigues <graziele.strodrigues@gmail.com>
2025-04-27 20:21:15 -0300

    Revisão de conceitos básicos

---
+++ mu_code/app.py
@@ -0,0 +1,45 @@
+# Importando as bibliotecas
+
+import streamlit as st
+import pandas as pd
+
+#Importando a penas a biblioteca express (mais rápida), que permite faça as coisas mais simplificada
+import plotly.express as px
+
+# Método set_page_config para configuração de layout
+
+st.set_page_config(layout="wide")
+
+#Criando as váriaveis e buscando os arquivos com o método read_csv
+# datasets/customer reviews.csv
+
+df_reviews = pd.read_csv("\Projetos-Python\Projeto web básico\datasets\customer reviews.csv")
+df_top100_books = pd.read_csv("C:\Users\Graziele Rodrigues\OneDrive\Documentos\MeusProjetos\Projetos-Python\Projeto web básico\datasets\Top-100 Trending Books.csv")
+
+# Criando o primeiro componente
+#Método .max() busca todos os valores da coluna e devolve apenas um único valor
+
+price_max = df_top100_books["book price"].max()
+price_min = df_top100_books["book price"].min()
+
+# st.sidebar = adiciona o componete a esquerda
+
+max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
+
+# Passando uma condição para ele filtrar
+# E "[]" vai permitir que ele aplique o filtro, dateframe final
+
+df_books = df_top100_books[df_top100_books["book price"] <= max_price]
+df_books # Chamando a váriavel para o streamlit entender que precisa print na tela
+
+# Criando Gráficos
+# .bar é um plotly do tipo bar
+fig = px.bar(df_books["year of publication"].value_counts())
+# Método px.histogram criará um histograma e calcula automaticamente
+fig2 = px.histogram(df_books["book price"])
+
+# Função st.colmns() devolve duas váriaies ao mesmo tempo
+
+col1, col2 = st.columns(2)
+col1.plotly_chart(fig)
+col2.plotly_chart(fig2)
---
+++ mu_code/pages/book reviews.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+
+# Método set_page_config para configuração de layout
+
+st.set_page_config(layout="wide")
+
+# Carrengando os arquivos
+
+df_reviews = pd.read_csv("C:\Users\Graziele Rodrigues\OneDrive\Documentos\MeusProjetos\Projetos-Python\Projeto web básico\datasets\customer reviews.csv")
+df_top100_books = pd.read_csv("C:\Users\Graziele Rodrigues\OneDrive\Documentos\MeusProjetos\Projetos-Python\Projeto web básico\datasets\Top-100 Trending Books.csv")
+
+# Criando uma lista de seleção de nome dos livros
+# Método unique() trará todos os nomes sem repetição
+
+books = df_top100_books["book title"].unique()
+book = st.sidebar.selectbox("Books", books)
+
+# Fazendo um filtro
+
+df_book = df_top100_books[df_top100_books["book title"] == book]
+df_reviews_f = df_reviews[df_reviews["book name"] == book]
+
+
+# iloc[0] trará apenas o primeiro índece que é string que precisamos
+book_title = df_book["book title"].iloc[0]
+book_genre = df_book["genre"].iloc[0]
+book_price = f"${df_book['book price'].iloc[0]}"
+book_rating = df_book["rating"].iloc[0]
+book_year = df_book["year of publication"].iloc[0]
+
+st.title(book_title)
+st.subheader(book_genre)
+col1, col2, col3 = st.columns(3)
+col1.metric("Price", book_price)
+col2.metric("Rating", book_rating)
+col3.metric("Year of Publication", book_year)
+
+st.divider()
+
+for row in df_reviews_f.values:
+    message = st.chat_message(f"{row[4]}")
+    message.write(f"**{row[2]}**")
+    message.write(row[5])
+
+
+
+
+
+
